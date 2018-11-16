1b1cb20c2500f097d3aa48e8c282473c594d8e33
Steven <stevenmi@live.de>
2018-11-16 13:35:02 +0100

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,8 +1,28 @@
+import urllib.request
 import feedparser
 
-def downloadPage(givenURL):
-    feed = feedparser.parse(givenURL)
+
+def download_html(url):
+    '''
+    This method get a URL and download the web page as HTML file
+    
+    :param url: URL to the web page as string
+    :return: HTML code as string
+    '''
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        page = response.read()
+    return page
+
+
+def download_rss(url):
+    '''
+    This method gets a URL and downloads the web page as XML file
+    
+    :param url: URL to the web page as string
+    :return: XML file as string
+    '''
+    feed = feedparser.parse(url)
     return feed
-    
-    
-    
+
+
